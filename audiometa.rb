acc4f5efca41922c17ee680c597a962c30861bdc
Rogerio Vicente <rogerpvl@gmail.com>
2010-01-03 01:27:03 +0000

    Removed old README. Added comments to Audiometa.

--- audiometa.rb
+++ audiometa.rb
@@ -6,6 +6,10 @@ module Audiometa
     attr_reader :artist
     attr_reader :track
     
+    # Checks if given file exists and if is supported,
+    # in this case it sets instance attributes, if not
+    # raises exception.
+    
     def initialize(file_path)
       @path = file_path
       raise 'File not found or unreadable.' unless File.exists? @path and File.readable? @path
@@ -17,11 +21,17 @@ module Audiometa
       @track = metadata['track']
     end
     
+    # Returns the file metadata in a hash
+    
     def parse_metadata
       bytes = get_last_bytes(128)
     end
     
     private
+    
+    # Returns the last n bytes of the file.
+    # This bytes should correspond to the location
+    # of the metadata info.
     
     def get_last_bytes(n)
       File.open(@path) do |f|
