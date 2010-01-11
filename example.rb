b04ffda1557bd91dee558313799c43d1865d3d87
Rogerio Vicente <rogerpvl@gmail.com>
2010-01-11 00:29:57 +0000

    Added gem packaging format.

---
+++ example.rb
@@ -0,0 +1,15 @@
+require 'tinyscrobbler.rb'
+
+ls = Tinyscrobbler::Client.new(lastfm_username, lasftfm_password)
+
+# Currently listening to...
+
+current_track = {'artistname' => 'Moonspell', 'track' => 'Alma Mater',
+  'time' => '1262185646', 'source' => 'P', 'rating' => '',
+  'secs' => '337', 'album' => 'Wolfheart', 'tracknumber' => '8', 'mbtrackid' => ''}
+
+ls.now_playing(current_track)
+
+# after track played
+
+ls.played(current_track)
