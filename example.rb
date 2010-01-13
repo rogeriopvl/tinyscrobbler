21d3178abc3fb4547214aaba1483cde7e8e823d8
Rogerio Vicente <rogerpvl@gmail.com>
2010-01-13 00:30:34 +0000

    Changed example to reflect recent changes.

--- example.rb
+++ example.rb
@@ -1,18 +1,22 @@
 require 'rubygems'
-require 'tinyscrobbler.rb'
+require 'tinyscrobbler'
 
 ls = Tinyscrobbler::Client.new(lastfm_username, lastfm_password)
 
 # Currently listening to...
 
 current_track = {'artistname' => 'Moonspell', 'track' => 'Alma Mater',
-  'time' => '1262185646', 'source' => 'P', 'rating' => '',
+  'time' => Time.now.to_s, 'source' => 'P', 'rating' => '',
   'secs' => '337', 'album' => 'Wolfheart', 'tracknumber' => '8', 'mbtrackid' => ''}
 
+# now playing notification
 ls.now_playing(current_track)
 
-# after track played
+# lastfm does not submit the track if
+# half track length has not passed
+sleep 170
 
+# after track played, lets submit
 ls.played(current_track)
 
 
